8461f70ea22f344bba1ae730fe821703a2085a5d
Lapey <bisisolesi@gmail.com>
2024-07-10 01:15:54 +0100

    added javascript to form

---
+++ app.js
@@ -0,0 +1,28 @@
+function emailSend(){
+
+        var name = document.getElementById('name').value;
+         var number = document.getElementById('number').value;
+          var email = document.getElementById('email').value;
+
+          var messageBody = " Name " +  name  +
+          "<br/> Number " + number +
+           "<br/> Email " + email; 
+           Email.send({
+            Host: "smtp.elasticemail.com",
+            Username: "bisisolesi@gmail.com",
+            Password: "E345F187A539FDEFF32F4E257D5479F89927",
+            To: 'thebekscorner@gmail.com',
+            From: "bisisolesi@gmail.com",
+            Subject: "This is the subject",
+            Body: "messageBody"
+        }).then(
+            message => {
+                if(message=='OK'){
+                    swal("Reserved!", "You clicked the button!",   "success");
+                }
+                else{
+                    swal("Try again!",  "error");
+                }
+            }
+        );
+    }
