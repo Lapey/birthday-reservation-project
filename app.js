c600cc21e2c9c214bf89343ef7aa9c6d9b81d58f
Lapey <bisisolesi@gmail.com>
2024-07-15 15:48:44 +0100

    Added footer

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ function emailSend(){
           var messageBody = " Name " +  name  +
           "<br/> Number " + number +
            "<br/> Email " + email; 
+
+           
            Email.send({
             Host: "smtp.elasticemail.com",
             Username: "bisisolesi@gmail.com",
             Password: "E345F187A539FDEFF32F4E257D5479F89927",
-            To: 'thebekscorner@gmail.com',
+            To: 'adebisisolesi@gmail.com',
             From: "bisisolesi@gmail.com",
             Subject: "This is the subject",
             Body: "messageBody"
